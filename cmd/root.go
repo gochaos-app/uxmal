b4c2fc157af1397d7051b7e12d037b7a55d07a77
RamonER <rodriguez.esparza.ramon@gmail.com>
2021-12-11 23:19:48 -0600

    test

--- cmd/root.go
+++ cmd/root.go
@@ -14,13 +14,9 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "uxmal",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A simple wrapper around k8s api to deploy and monitor jobs",
+	Long: `uxmal is a simple tool to deploy k8s jobs with no configuration or
+	complicated tools`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
