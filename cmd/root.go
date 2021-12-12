3d5200e775a7eca1e2cef59506f4d18721e513fa
RamonER <rodriguez.esparza.ramon@gmail.com>
2021-12-12 14:31:17 -0600

    completed CLI

--- cmd/root.go
+++ cmd/root.go
@@ -4,22 +4,16 @@ import (
 	"fmt"
 	"github.com/spf13/cobra"
 	"os"
-
-	homedir "github.com/mitchellh/go-homedir"
-	"github.com/spf13/viper"
 )
 
 var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "uxmal",
+	Use:   "uxmal [command]",
 	Short: "A simple wrapper around k8s api to deploy and monitor jobs",
-	Long: `uxmal is a simple tool to deploy k8s jobs with no configuration or
+	Long: `uxmal is a simple CLI to deploy k8s jobs with no configuration or
 	complicated tools`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -32,41 +26,5 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.uxmal.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".uxmal" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".uxmal")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
+	cobra.OnInitialize()
 }
--- main.go
+++ main.go
@@ -15,7 +15,7 @@ limitations under the License.
 */
 package main
 
-import "github.com/mental12345/uxmal"
+import "github.com/mental12345/uxmal/cmd"
 
 func main() {
 	cmd.Execute()
