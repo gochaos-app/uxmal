551c9987077e9963bde59e0e279239d2f73e8ca5
RamonER <rodriguez.esparza.ramon@gmail.com>
2021-12-12 19:55:54 -0600

    added k8s functions

--- cmd/create.go
+++ cmd/create.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	k8s "github.com/mental12345/uxmal/k8s/create"
 )
 
 // createCmd represents the create command
@@ -12,12 +13,11 @@ var createCmd = &cobra.Command{
 	Short: "creates a simple jobs given a set of parameters.",
 	Long: `uxmal create --name "job Name" --img "Image" --cmd "Command, you can use double quotes for commnds that requiere arguments"`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("create called")
 		name, _ := cmd.Flags().GetString("name")
 		image, _ := cmd.Flags().GetString("img")
 		command, _ := cmd.Flags().GetString("cmd")
 		script, _ := cmd.Flags().GetString("script")
-		fmt.Println(name, image, command, script)
+		k8s.K8sJobs(name, image, command)
 	},
 }
 
